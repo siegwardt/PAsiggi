75aa1f0464c7cf6ddf50f263ca0f3ae1702a45f4
Tizian Siegwardt <siegwardtt9@gmail.com>
2025-08-23 17:54:02 +0200

    implement sidebar and cart page

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,37 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import Layout from './Layout';
-import { AuthProvider } from './contexts/AuthContext';
-import './style/index.module.css';
-import Home from './pages/Home';
-import Konto from './pages/Konto';
-import Login from './pages/Login';
-import ProtectedKonto from './components/ProtectedKonto';
-import Shop from './pages/Shop';
-import Cart from './pages/Cart';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import Layout from "./Layout";
+import { AuthProvider } from "./contexts/AuthContext";
+import "./style/index.module.css";
+
+// Seiten
+import Home from "./pages/Home";
+import Konto from "./pages/Konto";
+import Login from "./pages/Login";
+import ProtectedKonto from "./components/ProtectedKonto";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+
+// 🛒 Warenkorb
+import { CartProvider } from "./components/shop/CartContext";
+import { CartSheet } from "./components/shop/CartSheet";
+import { CartFab } from "./components/shop/CartFab";
+
+// Kleine Hilfskomponente: blendet FAB auf /cart aus
+function GlobalCartUI() {
+  const location = useLocation();
+
+  // auf /cart nur Sidebar rendern, kein FAB
+  if (location.pathname === "/cart") {
+    return <CartSheet />;
+  }
+
+  return (
+    <>
+      <CartSheet />
+      <CartFab />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <BrowserRouter>
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="shop" element={<Shop />} />
-          <Route path="cart" element={<Cart />} />
-          <Route
-            path="konto"
-            element={<ProtectedKonto element={Konto} />}
-          />
-        </Route>
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="konto" element={<ProtectedKonto element={Konto} />} />
+          </Route>
+        </Routes>
+
+        {/* ✅ global: Warenkorb-UI */}
+        <GlobalCartUI />
+      </CartProvider>
     </AuthProvider>
   </BrowserRouter>
 );
 
-reportWebVitals()
+reportWebVitals();
